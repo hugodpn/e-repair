0c3dae20e0c67821a4a744825e5b854ec38c494f
Hugo Pacini <hugodpn@yahoo.com.ar>
2010-09-28 23:48:50 -0300

    Fix menus.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def selected_header_link selected
+    "class=\"activelink\"" if selected.include?(controller.controller_name)
+  end
 end
