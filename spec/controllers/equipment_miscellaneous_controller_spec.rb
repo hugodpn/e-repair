c65e5bbdc647b2063f1aa383b147e89b1cecd223
Hugo Pacini <hugodpn@yahoo.com.ar>
2010-11-08 19:28:51 -0300

    Add fixtures to spec test.

---
+++ spec/controllers/equipment_miscellaneous_controller_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe Admin::EquipmentMiscellaneousController do
+
+  fixtures :users
+  setup :activate_authlogic # run before tests are executed
+
+  before(:each) do
+    UserSession.create(users(:admin)) # logs a user in
+  end
+
+  describe "User Administrator" do
+
+    it "GET show equipmentes" do
+      assert_equal(true, false)
+    end
+
+  end
+
+end
