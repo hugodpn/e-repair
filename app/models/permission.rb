71b62839264ccc8ff03a956a55102390653de294
Hugo Pacini <hugodpn@yahoo.com.ar>
2011-01-25 20:50:11 -0300

    Complete permissions.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -31,54 +31,123 @@ class Permission < ActiveRecord::Base
   end
 
 
-#  PERMISSIONS = {
-#    :model => {
-#      :general => {
-#        :title => "Show model general something...",
-#        :columns => ["show", "edit", "create"],
-#        :rows => ["uno", "dos", "tres"]
-#      },
-#      :general_two => {
-#        :title => "Show model general_two something...",
-#        :columns => ["edit", "create"],
-#        :rows => ["uno", "dos", "tres"]
-#      }
-#    }, # "model_general_show_uno"
-#    :model_two => {
-#      :general => {
-#        :title => "Show model_two general something...",
-#        :columns => ["show", "edit", "create"],
-#        :rows => ["uno", "dos", "tres"]
-#      }
-#    }
-#  }
+  #  PERMISSIONS = {
+  #    :model => {
+  #      :general => {
+  #        :title => "Show model general something...",
+  #        :columns => ["show", "edit", "create"],
+  #        :rows => ["uno", "dos", "tres"]
+  #      },
+  #      :general_two => {
+  #        :title => "Show model general_two something...",
+  #        :columns => ["edit", "create"],
+  #        :rows => ["uno", "dos", "tres"]
+  #      }
+  #    }, # "model_general_show_uno"
+  #    :model_two => {
+  #      :general => {
+  #        :title => "Show model_two general something...",
+  #        :columns => ["show", "edit", "create"],
+  #        :rows => ["uno", "dos", "tres"]
+  #      }
+  #    }
+  #  }
 
 
   PERMISSIONS = {
-    :articles => {
-      :article => {
-        :title => "Articles Permissions",
+    :equipment_miscellaneous => {
+      :equipment_miscellaneous => {
+        :title => "Equipment miscellaneous",
         :columns => ["can"],
         :rows => ["list", "show", "update", "create", "destroy"]
       }
-    }, 
-    :users => {
-      :user => {
-        :title => "Users Permissions",
+    }, :request_repairs => {
+      :request_repairs => {
+        :title => "Request Repairs",
         :columns => ["can"],
         :rows => ["list", "show", "update", "create", "destroy"]
       }
-    },
-    :roles => {
-      :role => {
-        :title => "Roles Permissions",
+    }, :repairs => {
+      :repairs => {
+        :title => "Repairs",
         :columns => ["can"],
         :rows => ["list", "show", "update", "create", "destroy"]
       }
-    },
-    :categories => {
-      :category => {
-        :title => "Categories Permissions",
+    }, :report_repair_costs => {
+      :report_repair_costs => {
+        :title => "Report repair costs",
+        :columns => ["can"],
+        :rows => ["show"]
+      }
+    }, :report_take_out => {
+      :report_take_out => {
+        :title => "Report take out",
+        :columns => ["can"],
+        :rows => ["show"]
+      }
+    }, :report_request_repairs => {
+      :report_request_repairs => {
+        :title => "Report request repairs",
+        :columns => ["can"],
+        :rows => ["show"]
+      }
+    }, :report_repairs => {
+      :report_repairs => {
+        :title => "Report repairs",
+        :columns => ["can"],
+        :rows => ["show"]
+      }
+    }, :report_pending_repairs => {
+      :report_pending_repairs => {
+        :title => "Report pending repairs",
+        :columns => ["can"],
+        :rows => ["show"]
+      }
+    }, :report_repairs_by_equipment => {
+      :report_repairs_by_equipment => {
+        :title => "Report repairs by equipment",
+        :columns => ["can"],
+        :rows => ["show"]
+      }
+    }, :report_repairs_by_department => {
+      :report_repairs_by_department => {
+        :title => "Report repairs by department",
+        :columns => ["can"],
+        :rows => ["show"]
+      }
+    }, :departments => {
+      :departments => {
+        :title => "Department",
+        :columns => ["can"],
+        :rows => ["list", "show", "update", "create", "destroy"]
+      }
+    }, :equipment_types => {
+      :equipment_types => {
+        :title => "Equipment Types",
+        :columns => ["can"],
+        :rows => ["list", "show", "update", "create", "destroy"]
+      }
+    }, :equipment_locations => {
+      :equipment_locations => {
+        :title => "Equipment Locations",
+        :columns => ["can"],
+        :rows => ["list", "show", "update", "create", "destroy"]
+      }
+    }, :reason_faults => {
+      :reason_faults => {
+        :title => "Reason Faults",
+        :columns => ["can"],
+        :rows => ["list", "show", "update", "create", "destroy"]
+      }
+    }, :users => {
+      :users => {
+        :title => "Users",
+        :columns => ["can"],
+        :rows => ["list", "show", "update", "create", "destroy"]
+      }
+    }, :roles => {
+      :roles => {
+        :title => "Roles",
         :columns => ["can"],
         :rows => ["list", "show", "update", "create", "destroy"]
       }
